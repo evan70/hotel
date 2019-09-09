e64f25872f06ef6961420e16679f5b8a51436c02
evan70 <ivanpaldan@yandex.com>
2019-09-09 15:56:03 +0200

    added some files

---
+++ src/routes/_layout.svelte
@@ -0,0 +1,21 @@
+<script>
+  export let segment;
+
+  import { reservedRoom } from "../data/_stores.js";
+
+  import { onMount } from "svelte";
+  onMount(() => {
+    if (window.localStorage.getItem("roomsBooked"))
+      $reservedRoom = JSON.parse(localStorage.getItem("roomsBooked"));
+  });
+</script>
+
+<style lang="scss">
+  :global(html) {
+    font-size: 16px;
+    font-size: responsive 14px 19px; /* min-size, max-size */
+    font-range: 500px 1680px; /* viewport widths between which font-size is fluid */
+  }
+</style>
+
+<slot />
---
+++ src/routes/index.svelte
@@ -0,0 +1,67 @@
+<script>
+  import Slider from "../components/slider.svelte";
+  import Nav from "../components/navigation/nav_bottom.svelte";
+  import Logo from "../components/logo.svelte";
+  import Footer from "../components/_footer.svelte";
+  export let segment;
+</script>
+
+<style lang="scss">
+  h1 {
+    text-align: center;
+    color: inherit;
+    font-size: 2.8em;
+    text-transform: uppercase;
+    margin: 0 0 0.5em 0;
+  }
+  section {
+    position: relative;
+  }
+  .nav,
+  .logo {
+    position: absolute;
+    z-index: 100;
+  }
+  .nav {
+    bottom: 0;
+    left: 0;
+    width: 100%;
+  }
+  .logo {
+    left: 50%;
+    transform: translateX(-50%) scale(1.4);
+    top: 10vh;
+  }
+  .container :global(footer) {
+    padding-top: 5rem;
+    padding-bottom: 2rem;
+    font-size: 1rem;
+    background: #2172eb;
+    color: #f2f2f2;
+  }
+</style>
+
+<svelte:head>
+  <title>permon hotel ****</title>
+</svelte:head>
+
+<div class="container">
+  <section>
+    <div class="logo">
+      <Logo {segment} />
+    </div>
+    <Slider>
+      <h1>permon hotel ****</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad iusto quasi
+        eligendi deleniti officia, dolores enim facere dignissimos
+        reprehenderit, nobis et soluta ex vel quas perspiciatis mollitia
+        nostrum. Hic, molestias.
+      </p>
+    </Slider>
+    <div class="nav">
+      <Nav {segment} />
+    </div>
+  </section>
+  <Footer />
+</div>
