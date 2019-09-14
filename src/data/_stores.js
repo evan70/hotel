26207949bf76555709c298ebc9977eb76ad88ffb
evan70 <ivanpaldan@yandex.com>
2019-09-14 19:54:08 +0200

    added _stores.js file

---
+++ src/data/_stores.js
@@ -0,0 +1,6 @@
+import { writable } from 'svelte/store'
+
+export const reservedRoom = writable([])
+export const roomsFilter = writable({})
+export const navHeight = writable()
+export const slugTitle = writable()
